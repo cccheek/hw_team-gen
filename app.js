1896679501d31a035be6f009cda9c94ce066eae9
Claire Cheek <claireccheek@gmail.com>
2021-01-12 22:44:21 -0800

    first inquirer block

--- app.js
+++ app.js
@@ -29,11 +29,17 @@ inquirer.prompt([
  } else {
      chooseIntern();
  }
+ function chooseManager(){
+    inquirer.prompt()
+ }
+
 })
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -41,9 +47,13 @@ inquirer.prompt([
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
+
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
+
+
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
