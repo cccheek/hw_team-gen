51450de913ac9fda57ffda502238ca9322339242
Claire Cheek <claireccheek@gmail.com>
2021-01-13 22:30:26 -0800

    bugged out

--- app.js
+++ app.js
@@ -8,46 +8,176 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+
 const render = require("./lib/htmlRenderer");
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-inquirer.prompt([
-    {
-        type: "list",
-        name: "type",
-        message: "What type of employee?",
-        choices: ["Manager", "Engineer", "Intern"]
-    }
-]).then(select => {
- if (select.type === Engineer){
-     chooseEngineer();
- } else if (select.type === Manager){
-     chooseManager();
- } else {
-     chooseIntern();
- }
- function chooseManager(){
-    inquirer.prompt()
- }
-
-})
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
 
+const employeeList = [];
+let HTML = "";
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
+const engineerPrompt = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your Engineer's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your Engineer's ID number?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your Engineer's email?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your Engineer's GitHub?"
+    }
+]
+
+const managerPrompt = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your Manager's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your Manager's ID number?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your Manager's email?"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your Manager's office number?"
+    }
+]
+
+
+const internPrompt = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your Intern's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your Intern's ID number?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your Intern's email?"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is your Intern's school?"
+    }
+]
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+const addMore = () => {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            message: "Would you like to add another?",
+            name: "addMore"
+        }
+    ]).then(select => {
+
+        if (select.addMore === true) {
+            console.log("Sure thing!");
+            employeeChoice();
+        } else {
+            HTML += render(employeeList);
+            fs.writeFile(outputPath, HTML, err => console.log(err))
+            console.log("You're all done!");
+            return;
+        }
+    })
+}
+
+
+const employeeChoice = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "type",
+            message: "What type of employee would you like to add?",
+            choices: ["Manager", "Engineer", "Intern"]
+        }
+    ]).then(select => {
+        if (select.type === "Manager") {
+            promptManager();
+        } else if (select.type === "Engineer") {
+            promptEngineer();
+        } else {
+            promptIntern();
+        }
+
+        function promptManager()  {
+            inquirer.prompt(managerPrompt)
+                .then(select => {
+
+                    const employee = new Manager(select.name, select.id, select.email, select.officeNumber);
+                    employeeList.push(employee);
+                    addMore();
+                })
+                .catch(err => console.log(err));
+        }
+
+        function promptEngineer()  {
+            inquirer.prompt(engineerPrompt)
+                .then(select => {
+
+                    const employee = new Engineer(select.name, select.id, select.email, select.github);
+                    employeeList.push(employee);
+                    addMore();
+                })
+                .catch(err => console.log(err));
+        }
+
+        function promptIntern()  {
+            inquirer.prompt(internPrompt)
+                .then(select => {
+
+                    const employee = new Intern(select.name, select.id, select.email, select.school);
+                    employeeList.push(employee);
+                    addMore();
+                })
+                .catch(err => console.log(err));
+        }
+
+    })
+
+
+}
+
+
+
+employeeChoice();
+
+
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
-
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
